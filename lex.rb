cb81ebd4e7f599b594abd8766561a8ba60743dd7
rb001 <rohini_behl@berkeley.edu>
2015-03-25 16:48:30 -0700

    final commit

--- lex.rb
+++ lex.rb
@@ -4,26 +4,121 @@ require './words'
 
 # Detects whether a the given word is a palindrome
 def is_palindrome?(word)
-  # FILL ME IN
-  return false
+  if word == nil or word.lstrip().empty?
+    return false
+  end
+
+  i = 0
+  j = word.length - 1
+  while (i < j && j < word.length)
+    if (word[i] == word[j])
+      i+=1;
+      j-=1;
+    else
+      return false
+    end
+  end
+  return true
 end
 
 # Generates an array of all the anagrams of the given word
 def generate_anagrams(word)
-  # FILL ME IN
-  return []
+  if word == nil or word.lstrip().empty?
+    return []
+  end
+  if word.length == 1
+    return [word]
+  end
+  Lexicon.generateAnagrams()
+  anagrams = Lexicon.anagrams
+  key = word.chars.sort { |a, b| a.casecmp(b) } .join.downcase
+  if anagrams.has_key?(key)
+    return anagrams[key]
+  else
+    return []
+  end
+end
+
+#Binary search between head and tail indices in lexicon array. 
+#Starting represents the start index of our desired prefix in the lexicon array,
+# which is returned when type=0 will return.
+#Ending represents the end index of our desired prefix in the lexicon array,
+# which is returned when type=1.
+def binary_search(starting, ending, head, tail, prefix, type, lexicon)
+  while (head <= tail) 
+    midpoint = (tail - head)/2 + head
+    word = lexicon[midpoint].downcase
+    result = word <=> prefix
+    if (word.start_with? prefix or result == 0)
+      if type == 0
+        starting = midpoint
+        tail = midpoint - 1
+      else
+        ending = midpoint
+        head = midpoint + 1
+      end
+    elsif (result == -1)
+      head = midpoint + 1
+    else
+      tail = midpoint - 1
+    end
+  end
+  if type == 0
+    return starting
+  else
+    return ending
+  end
 end
 
 # Generates an array of all the words that have the given word as a prefix
 def prefixed_words(prefix)
-  # FILL ME IN
-  return []
+  if prefix == nil or prefix.lstrip().empty?
+    return []
+  end
+
+  lexicon = Lexicon.array
+  prefix = prefix.downcase
+  head = 0
+  tail = lexicon.length-1
+  starting = -1
+  ending = -1
+  starting = binary_search(starting, ending, head, tail, prefix, 0, lexicon)
+  head = starting
+  tail = lexicon.length - 1
+  ending = binary_search(starting, ending, head, tail, prefix, 1, lexicon)
+   
+  if (starting == -1 && ending == -1)
+    return []
+  end
+
+  prefixed = []
+  for i in starting..ending
+    prefixed.push(lexicon[i])
+  end
+  return prefixed
 end
 
-# Generates the shortest possible word ladder connecting the two words
+# Generates the shortest possible word ladder connecting the two words. 
+#Creates a graph with edges between words that are one character apart 
+#(no adding or deletion of characters just a change) and runs Dijkstra's
+#on it. Note: If this were not an interview question, I would use something like
+#Ruby Tree or some other built-in module to generate and traverse a graph.
 def word_ladder(start_word, end_word)
-  return []
+  graph = Graph. new()
+  bucketToWords = Lexicon.buckets
+  for bucket in bucketToWords.keys()
+    for word1 in bucketToWords[bucket]
+      for word2 in bucketToWords[bucket]
+        if word1 != word2
+          graph.add_vertex(word1, word2)
+        end
+      end
+    end
+  end
+  return graph.shortest_path(start_word, end_word)
 end
+
+
 
 
 # Main program body -- this is what ends up on the terminal
@@ -55,9 +150,14 @@ print 'Please enter an end word: '
 end_word = gets.chomp
 ladder = word_ladder(word, end_word)
 
+
 puts "\nHere is the word ladder for #{word} -> #{end_word}:"
-print "#{word} -> "
-ladder.each do |rung|
-  print "#{rung} -> "
+if ladder.length == 1 and ladder[0] == 0
+  puts "\n     There is no possible word ladder for #{word} -> #{end_word}"
+else 
+  print "#{word} -> "
+  ladder.each do |rung|
+    print "#{rung} -> "
+  end
+  puts end_word
 end
-puts end_word
--- words.rb
+++ words.rb
@@ -1,9 +1,12 @@
 #!/usr/bin/ruby
-
+require 'set'
+require 'priority_queue'
 
 class Lexicon
   @@hash = nil
   @@array = nil
+  @@anagrams = nil
+  @@bucketToWords = nil
 
   # Returns true if the given word is in the lexicon
   def self.is_word?(word)
@@ -33,12 +36,119 @@ class Lexicon
   def self.read
     @@array = []
     @@hash = {}
+    @@bucketToWords = Hash.new { |h, k| h[k] = [] }
     file = File.new('words.txt', 'r')
     while (line = file.gets)
       line.strip!.downcase!
       @@array.push line
       @@hash[line] = true
+
+      #Generate buckets of words with only one character different
+      # between words in a single bucket to create the graph for
+      # running Dijkstra's on
+      n = line.length - 1
+      bucket = '_'.to_str << line[1..n].to_str
+      if @@bucketToWords.has_key? bucket
+        @@bucketToWords[bucket].push(line)
+      else
+        @@bucketToWords[bucket] = [line]
+      end
+      for i in 1..n
+        bucket = line[0..i-1] << '_' << line[i+1..n]
+        if @@bucketToWords.has_key? bucket
+          @@bucketToWords[bucket].push(line)
+        else
+          @@bucketToWords[bucket] = [line]
+        end
+      end
     end
     file.close
+  end
+
+  def self.anagrams
+    @@anagrams
+  end
+
+  def self.buckets
+    @@bucketToWords
+  end
+
+  #Generates a map of key - lowercase alphabetically sorted version
+  #of word, to value - list of words with that key (i.e. anagrams of that word) 
+  def self.generateAnagrams
+    @@anagrams = Hash.new { |h, k| h[k] = Set.new }
+    @@array.each do |entry|
+      key = entry.chars.sort { |a, b| a.casecmp(b) } .join.downcase
+      if !@@anagrams.has_key? key
+        @@anagrams[key] = [entry].to_set
+      else 
+        @@anagrams[key].add(entry)
+      end
+    end
+  end
+end
+
+class Graph
+  def initialize
+    @@vertexToEdges = Hash.new { |h, k| h[k] = [] }
+  end
+
+  #Adds an edge to the @@vertexToEdges adjacency list representation
+  #of the graph. 
+  def add_vertex(vertex, edges)
+    if @@vertexToEdges.has_key? vertex
+      @@vertexToEdges[vertex].push(edges)
+    else
+      @@vertexToEdges[vertex] = [edges]
+    end
+  end
+
+  #Dijkstra's shortest path between start and target
+  def shortest_path(start, target)
+    maxint = Float::INFINITY
+    distances = {}
+    previous = {}
+    q = PriorityQueue.new
+    
+    @@vertexToEdges.keys().each do |vertex|
+      if vertex == start
+          distances[vertex] = 0
+          q[vertex] = 0
+      else
+          distances[vertex] = maxint
+          q[vertex] = maxint
+      end
+      previous[vertex] = nil
+    end
+    
+    while q
+        smallest = q.delete_min_return_key
+        if smallest == target
+          path = []
+          while previous[smallest]
+              path.push(smallest)
+              smallest = previous[smallest]
+          end
+          path = path.reverse[0..path.length-2]
+          if path.length == 1 and path[0]== target
+            return []
+          end
+          return path
+        end
+            
+        if smallest == nil or distances[smallest] == maxint
+          break            
+        end
+
+        @@vertexToEdges[smallest].each do |neighbor|
+            alt = distances[smallest] + 1
+            if alt < distances[neighbor]
+                distances[neighbor] = alt
+                previous[neighbor] = smallest
+                q[neighbor] = alt
+            end
+        end
+    end
+    return [0]
   end
 end
